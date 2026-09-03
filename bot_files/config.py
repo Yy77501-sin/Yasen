f167bdc1edcd5ced884be36b9743b98c70ce6f2f
Yy77501-sin <yasenyemeni00@gmail.com>
2026-09-04 02:09:09 +0300

    Update config.py

--- bot_files/config.py
+++ bot_files/config.py
@@ -9,7 +9,7 @@ import os
 # Telegram Bot
 # =========================================================
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8657439320:AAFTQ-JxlBAfRyaP2yWnjO_4IjpuXdrA3Rc").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8880668064:AAHFsGU8w_Hzt0-2bxmGB3yDKVwcuPQMiIA").strip()
 
 # ضع ADMIN_ID في Render/Railway كمتغير بيئة.
 # مثال: ADMIN_ID=123456789
